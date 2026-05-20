5844f8f53d1f76d02418dc3510e0882a6cc24076
Faner <fanerstder@gmail.com>
2026-05-19 21:22:06 -0500

    actualizando archivo application-docker.yml

--- backend/src/main/java/com/talentcircle/domain/port/out/DraftRepository.java
+++ backend/src/main/java/com/talentcircle/domain/port/out/DraftRepository.java
@@ -3,10 +3,12 @@ package com.talentcircle.domain.port.out;
 import com.talentcircle.domain.model.Draft;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DraftRepository extends JpaRepository<Draft, String> {
     List<Draft> findByExecutionId(String executionId);
     List<Draft> findByStatus(com.talentcircle.domain.model.Draft.DraftStatus status);
+    List<Draft> findByStatusAndCreatedAtBetween(Draft.DraftStatus status, LocalDateTime start, LocalDateTime end);
 }
