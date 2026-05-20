a59a6fd9a626b0c55544ca896bb0d7abba25579f
Faner <fanerstder@gmail.com>
2026-05-19 21:39:18 -0500

    actualizando

--- backend/src/main/resources/db/migration/h2/V9__create_message_entity.sql
+++ backend/src/main/resources/db/migration/h2/V9__create_message_entity.sql
@@ -1,7 +1,7 @@
 CREATE TABLE message_entity (
-                                id BIGSERIAL PRIMARY KEY,
-                                discord_id VARCHAR(50) NOT NULL,
-                                author VARCHAR(100) NOT NULL,
-                                content TEXT,
-                                published_at TIMESTAMP
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    discord_id VARCHAR(50) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    content TEXT,
+    published_at TIMESTAMP
 );
